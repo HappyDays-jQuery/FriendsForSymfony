6853649445db772582903792b960d6b642018bd2
HappyDays-jQuery <developer@jquery.tokyo>
2016-06-25 17:44:45 +0900

    doctorineでメソッド追加
    php bin/console doctrine:generate:entities Acme

--- src/Acme/StoreBundle/Entity/Category.php
+++ src/Acme/StoreBundle/Entity/Category.php
@@ -73,5 +73,38 @@ class Category
     {
         return $this->name;
     }
-}
 
+    /**
+     * Add product
+     *
+     * @param \Acme\StoreBundle\Entity\Product $product
+     *
+     * @return Category
+     */
+    public function addProduct(\Acme\StoreBundle\Entity\Product $product)
+    {
+        $this->products[] = $product;
+
+        return $this;
+    }
+
+    /**
+     * Remove product
+     *
+     * @param \Acme\StoreBundle\Entity\Product $product
+     */
+    public function removeProduct(\Acme\StoreBundle\Entity\Product $product)
+    {
+        $this->products->removeElement($product);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+}
