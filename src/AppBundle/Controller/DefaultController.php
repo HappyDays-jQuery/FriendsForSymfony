43d9e9c0be1dad7de77a68e9fae620a3826ef1dd
HappyDays-jQuery <developer@jquery.tokyo>
2016-06-25 11:50:54 +0900

    未使用変数

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,10 +15,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        var_dump($request);
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'request' => $request,
         ]);
     }
 }
