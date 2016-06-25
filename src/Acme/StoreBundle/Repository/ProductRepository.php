84c54eed30f4873c77d042654d1fd1b5f7a644ce
HappyDays-jQuery <developer@jquery.tokyo>
2016-06-25 17:37:05 +0900

    findAllOrderByName追加

--- src/Acme/StoreBundle/Repository/ProductRepository.php
+++ src/Acme/StoreBundle/Repository/ProductRepository.php
@@ -12,4 +12,9 @@ use Doctrine\ORM\EntityRepository;
  */
 class ProductRepository extends EntityRepository
 {
+    public function findAllOrderedByName()
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT p FROM AcmeStoreBundle:Product p ORDER BY p.name ASC');
+    }
 }
