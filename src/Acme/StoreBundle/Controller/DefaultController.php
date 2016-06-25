5dde2a8e26a65b7831e026cf3962602a4540c685
HappyDays-jQuery <developer@jquery.tokyo>
2016-06-25 17:30:38 +0900

    コントローラーにread追加

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -33,4 +33,44 @@ class DefaultController extends Controller
 
         return new Response('Created product id ' . $product->getId());
     }
+
+    /**
+     * @Route("/store/product/{id}")
+     * @param integer $id
+     */
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('AcmeStoreBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id ' . $id);
+        }
+
+        /*
+        // プライマリーキー(通常は"id")でクエリ
+        $product = $repository->find($id);
+
+        // あるカラム値に基づいて find する、動的なメソッド名
+        $product = $repository->findOneById($id);
+        $product = $repository->findOneByName('foo');
+
+        // *すべて* の商品を find
+        $products = $repository->findAll();
+
+        // 任意のカラム値に基づく、商品群の find
+        $products = $repository->findByPrice(19.99);
+
+        // name と price の両方にマッチする1つの商品を取得するクエリ
+        $product = $repository->findOneBy(array('name' => 'foo', 'price' => 19.99));
+
+        // name にマッチするすべての商品を price 順で取得するクエリ
+        $product = $repository->findBy(
+            array('name' => 'foo'),
+            array('price' => 'ASC')
+        );
+        */
+        // do something, like pass the $product object into a template
+    }
 }
