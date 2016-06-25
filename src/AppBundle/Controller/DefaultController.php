08b506dd11a3066764053e2cc39caae9807fda96
HappyDays-jQuery <developer@jquery.tokyo>
2016-06-25 10:16:45 +0900

    ci設定調整

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,6 +15,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        var_dump($request);
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
